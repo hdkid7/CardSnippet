fa37480f4e4e64e2ed153b729d894922ba396e75
hdkid7 <hameddiakite34@yahoo.com>
2020-04-03 17:00:03 -0400

    base design for mobile added

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import Swiper from "swiper";
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'flip',
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  console.log("hell")
