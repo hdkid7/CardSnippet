666698019855b94339e93b193c7b4512aeeeb8ef
hdkid7 <hameddiakite34@yahoo.com>
2020-04-08 16:54:24 -0400

    added page views

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,44 @@
 import Swiper from "swiper";
 
-var swiper = new Swiper('.swiper-container', {
-    effect: 'flip',
-    grabCursor: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+var swiper = new Swiper(".swiper-container", {
+  effect: "flip",
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  console.log("hell")
+const addActiveClass = (element) => element.classList.add("active");
+const removeActiveClass = (element) => element.classList.remove("active");
+
+const header = document.getElementById("header");
+const linksInHeader = header.querySelectorAll(".link");
+
+const displayPage = (pageElement) => {
+  pageElement.style.display = "block";
+};
+
+const getElementId = (element) => document.getElementById(element);
+
+const removePage = (pageElement) => {
+  pageElement.style.display = "none";
+};
+
+const toggleActive = () => {
+  linksInHeader.forEach((link) => {
+    link.onclick = () => {
+      toggleActive();
+      addActiveClass(link);
+      displayPage(getElementId(link.getAttribute("data-value")));
+    };
+    removeActiveClass(link);
+    removePage(getElementId(link.getAttribute("data-value")));
+  });
+};
+toggleActive();
+
+getElementId("staffTag").click();
