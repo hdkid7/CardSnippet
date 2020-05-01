dca15565b5ce0320ed28a57df679a85be84c7a5a
Paul Adu-Gyamfi <pjadu7@gmail.com>
2020-05-01 10:30:24 -0400

    changed from swipe to hover

--- src/js/ViewModel.js
+++ src/js/ViewModel.js
@@ -1,27 +1,24 @@
 const createViewModel = (cardObject) => {
     const element =
     `
-    <div class="card">
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide" style="background-image:linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.81)), url('${cardObject.imagePath}')" mix-blend-mode:="multiply">
+  <div class="card">
+    <div class="card-inner">
+        <div class="card-front" style="background-image:linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.81)), url('${cardObject.imagePath}')" mix-blend-mode:"multiply">
           <div class="caption">
             <p id="name">${cardObject.name}</p>
             <p id="position">${cardObject.profession}</p>
-            
           </div>
         </div>
-        <div class="swiper-slide">
-          <div class="back-info">
-            <div class="info"><span>Phone:</span> <br>${cardObject.phoneNumber} </div>
-            <div class="info">${cardObject.roomNumber}</div>
-            <div class="info"><span>Website (URL):</span> ${cardObject.webUrl}</div>
-            <div class="info"><span>Email:</span> ${cardObject.email}</div>
-            <div class="info" id="skills">${cardObject.description}</div>
-            <div><a src=""></a><button type="button" onclick="location.href = 'http://${cardObject.readMoreLink}';">Read More</a></button></div>
-          </div>
+  
+        <div class="card-back">
+          <div class="info"><span>Phone:</span> <br>${cardObject.phoneNumber} </div>
+          <div class="info">${cardObject.roomNumber}</div>
+          <div class="info"><span>Website (URL):</span> ${cardObject.webUrl}</div>
+          <div class="info"><span>Email:</span> ${cardObject.email}</div>
+          <div class="info" id="skills">${cardObject.description}</div>
+          <div><a src=""></a><button type="button" onclick="location.href = 'http://${cardObject.readMoreLink}';">Read More</a></button></div>
         </div>
-      </div>
+  
     </div>
    </div>
    `
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,23 @@
-import Swiper from "swiper";
+
 import CardBuilder from "./CardBuilder";
 import {cardArray,userCardMaker} from "./CardArray"
 import createViewModel from "./ViewModel"
 
 
-userCardMaker({name:"John Allen",profession:"Blacksmith",email:"jack@yahoo.com",roomNumber:"224"})
-userCardMaker({name:"Jack slals",profession:"Grren Man"})
-userCardMaker({name:"Jack Black",profession:"Grren Man",readMoreLink:"google.com"})
+
+userCardMaker({name:"Stephen Curry",profession:"NBA Player",email:"jack@yahoo.com",roomNumber:"224", phoneNumber:"(718)-435-45656", imagePath:"https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY4MTg4NzMwNzU3MDk3MzQ1/stephen-curry-unlikely-return-injury.jpg"})
+userCardMaker({name:"Lil Tjay",profession:"Rapper", imagePath:"https://memberdata.s3.amazonaws.com/hi/hitsdd/photos/hitsdd_photo_gal__photo_1839800802.jpg"})
+userCardMaker({name:"Lil Tecca",profession:"Rapper",readMoreLink:"google.com", imagePath:"https://static01.nyt.com/images/2019/10/06/arts/music/doas-tecca-promo/GettyImages-1165612474-square640.jpg"})
+userCardMaker({name:"Lebron James",profession:"NBA Player", imagePath:"https://nba.nbcsports.com/wp-content/uploads/sites/12/2020/04/GettyImages-1206647160-e1586390067316.jpg?w=610&h=343&crop=1"})
+userCardMaker({name:"Ninja",profession:"Streamer", imagePath:"https://api.time.com/wp-content/uploads/2019/04/tyler-blevins-ninja-time-100-2019-002-1.jpg?quality=85&zoom=2"})
+userCardMaker({name:"Shiggy",profession:"Comedian", imagePath:"https://static.vibe.com/files/2018/08/GettyImages-1001042940-1533829844-compressed.jpg"})
+userCardMaker({name:"Donald Trump",profession:"US President", imagePath:"https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg"})
+userCardMaker({name:"Lamelo Ball",profession:"Basketball Player", imagePath:"https://images2.minutemediacdn.com/image/fetch/c_fill,g_auto,f_auto,h_2133,w_3200/https%3A%2F%2Fpippenainteasy.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2016%2F04%2F1190997301-1.jpeg"})
+
+
+
+
+
 
 cardArray
 .map(item => createViewModel(item))
@@ -15,17 +26,7 @@ cardArray
 })
 
 
-var swiper = new Swiper(".swiper-container", {
-  effect: "flip",
-  grabCursor: true,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+
 
 const addActiveClass = (element) => element.classList.add("active");
 const removeActiveClass = (element) => element.classList.remove("active");
